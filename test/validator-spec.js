348370aa093e16b1d7c7fe6af4595665dd35ea71
askl56 <amscott@openmailbox.org>
2015-04-25 13:46:31 +0100

    learning BDD with node/express

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,6 +1,9 @@
-var assert = require('assert');
-   describe('Be welcome to Mocha', function() {
-     it('with a failing test', function() {
-       assert(false, 'Hello World');
-     });
+var chai = require('chai');
+expect = chai.expect,
+    validator = require('../lib/validator');
+validator = require('../lib/validator');
+describe('A validator', function(){
+   it('will return error.nonpositive for not strictly positive numbers', function() {
+       expect(validator(0).to.be.deep.equal(['error.nonpositive']);
+   });
 });
